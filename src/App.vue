94af31ca54630f777ced095498b86465c29c4268
cuibaoxin <zwebcui@163.com>
2022-02-23 21:00:48 +0800

    element完整引入, 按需引入对比

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,25 @@
     <button @click="test">测试</button>
     <!-- 跟路由出口 -->
     <router-view/>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-  interface Foo {
-    a: string
-    b: number
-  }
-  interface Fo {a: string; b: number}
+// interface Foo {
+//   a: string
+//   b: number
+// }
+// interface Fo {a: string; b: number}
 
 // 这种类的方式,暂时还没有定版, 所以不使用
 // import Component from 'vue-class-component'
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 完整引入
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// Vue.use(ElementUI)
+
+// 按需引入 按照了包 npm install babel-plugin-component -D
+import '@/lib/elementui'
 
 Vue.config.productionTip = false
 
